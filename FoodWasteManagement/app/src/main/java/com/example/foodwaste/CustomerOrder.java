4d8e0ac675252a5e861226bcf475b45f10f2557b
Kediyal <kediyalr@student.douglascollege.ca>
2023-03-29 21:32:35 -0700

    Need to Add Custom Adapter, Simple Adapter not Working

--- FoodWasteManagement/app/src/main/java/com/example/foodwaste/CustomerOrder.java
+++ FoodWasteManagement/app/src/main/java/com/example/foodwaste/CustomerOrder.java
@@ -2,12 +2,14 @@ package com.example.foodwaste;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.os.Bundle;
 
-import com.google.gson.Gson;
+import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class CustomerOrder extends AppCompatActivity {
 
@@ -16,6 +18,44 @@ public class CustomerOrder extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_order);
         ArrayList<Menu> menu = (ArrayList<Menu>) getIntent().getSerializableExtra("MenuItem");
+        ArrayList<HashMap<String,String>> aList =
+                new ArrayList<HashMap<String,String>>();
+        String[] ItemName =  new String[menu.size()];
+        String[] ItemQty = new String[menu.size()];
+        String[] ItemPrice = new String[menu.size()];
+
+
+         for(int i=0;i<menu.size();i++)
+         {
+
+             ItemName[i] = menu.get(i).itemName;
+             ItemQty[i] = menu.get(i).Qty;
+             ItemPrice[i] = menu.get(i).price;
+
+         }
+
+
+        for(int i=0;i<menu.size();i++)
+         {
+             HashMap<String,String> hashMap = new HashMap<>();
+              hashMap.put("name",ItemName[i]);
+              hashMap.put("qty",ItemQty[i]);
+              hashMap.put("price",ItemPrice[i]);
+             aList.add(hashMap);
+
+         }
+
+        String[] from = {"name","qty","price"};
+        int[] to = {R.id.ItemName,R.id.ItemQty,R.id.ItemPrice};
+
+        System.out.println("List:"+aList);
+
+        SimpleAdapter adapter = new SimpleAdapter(CustomerOrder.this,
+                aList,R.layout.bill_generation,from,to);
+
+        ListView listView = findViewById(R.id.listview);
+
+        listView.setAdapter(adapter);
 
 
 
